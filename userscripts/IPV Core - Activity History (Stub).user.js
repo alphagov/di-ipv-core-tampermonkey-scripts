9cc5d9218605f93c1a9e563b45b2d67758172ca4
Daniel Pomfret <daniel.pomfret@digital.cabinet-office.gov.uk>
2022-05-06 10:32:30 +0100

    Add includes for other environments

--- userscripts/IPV Core - Activity History (Stub).user.js
+++ userscripts/IPV Core - Activity History (Stub).user.js
@@ -2,7 +2,7 @@
 // @name         IPV Core - Activity History (Stub)
 // @namespace    http://gov.uk
 // @version      0.1
-// @description  Mocks our Activity History (Stub)
+// @description  Mocks our Activity History
 // @author       Daniel Pomfret
 // @match        https://staging-di-ipv-cri-activity-history-stub.london.cloudapps.digital/authorize*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cloudapps.digital
--- userscripts/IPV Core - Knowlegde Based Verification (Stub).user.js
+++ userscripts/IPV Core - Knowlegde Based Verification (Stub).user.js
@@ -3,7 +3,7 @@
 // @namespace    http://gov.uk
 // @version      0.1
 // @description  Mocks out Knowlegde Based Verification (Stub)
-// @author       You
+// @author       Daniel Pomfret
 // @match        https://staging-di-ipv-cri-kbv-stub.london.cloudapps.digital/authorize*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cloudapps.digital
 // @grant        none
